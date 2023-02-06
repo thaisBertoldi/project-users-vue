21f3e40dda911902a4f1dfdd57d24aba506b0727
Thais B <thais_brt@hotmail.com>
2023-02-06 17:46:37 -0300

    feat: pagina de login

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
+import LoginView from '../views/LoginView.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/register',
     name: 'register',
     component: RegisterView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: LoginView
   },
   {
     path: '/about',
---
+++ src/views/LoginView.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="container">
+        <h2 class="title-register">Login</h2>
+        <div class="column is-centered form-input box">
+            <div v-if="error">
+                <p class="error">{{ error }}</p>
+            </div>
+            <input v-model="email" type="email" placeholder="Email" class="input">
+            <input v-model="password" type="password" placeholder="********" class="input">
+            <button class="button is-success" @click="login">Entrar</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import { notify } from 'vuejs-notify'
+import axios from 'axios'
+
+export default {
+    data() {
+        return {
+            password: '',
+            email: '',
+            error: undefined
+        }
+    },
+    methods: {
+        login() {
+            axios.get('http://localhost:8686/user').then(() => {}).catch(err => {
+                notify.error({
+                    position: 'top center',
+                    title: 'Não foi possível realizar o login.',
+                    timeout: 10000
+                });
+                this.error = err.response.data.err;
+            })
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+    .container {
+        margin-top: 5%;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .form-input {
+        display: flex;
+        flex-direction: column;
+        gap: 15px;
+        padding: 50px;
+    }
+
+    .div-select {
+        display: flex;
+        flex-direction: column;
+    }
+
+    .title-register {
+        color: #f2f3f3;
+        padding: 10px 0;
+        font-weight: 400;
+        font-size: 20px;
+        line-height: 1.125;
+        background-color: rgb(125, 128, 125);
+        border-top-left-radius: 6px;
+        border-top-right-radius: 6px;
+    }
+
+    .image-anotation {
+        position: absolute;
+        width: 150px;
+        left: 93%;
+        top: -20px;
+    }
+
+    .error {
+        color: red;
+    }
+
+</style>
